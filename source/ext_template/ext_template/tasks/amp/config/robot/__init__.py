e7f9c172c012da39ce2e625f9bc4af26532014dc
yoosunkyum <89465476+yoosunkyum@users.noreply.github.com>
2025-06-18 17:32:03 +0900

    Update __init__.py

--- source/ext_template/ext_template/tasks/amp/config/robot/__init__.py
+++ source/ext_template/ext_template/tasks/amp/config/robot/__init__.py
@@ -5,14 +5,22 @@ from . import agents, env_cfg
 ##
 # Register Gym environments.
 ##
-
 gym.register(
-    id="Isaac-Robot-AMP-v0",
+    id="Template-Isaac-AMP-G1-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": env_cfg.RobotAmpEnvCfg,
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.amp_rsl_rl_cfg:RobotAmpOnPolicyRunnerCfg",
+        "env_cfg_entry_point": env_cfg.G1AmpEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.amp_rsl_rl_cfg:G1AmpRunnerCfg",
     },
 )
 
+gym.register(
+    id="Template-Isaac-AMP-H1-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": env_cfg.H1AmpEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.amp_rsl_rl_cfg:H1AmpRunnerCfg",
+    },
+)
